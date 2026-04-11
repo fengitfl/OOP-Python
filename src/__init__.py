2087d1d372786f5b6470ef412664485c5a5e981b
fengitfl <enikonov229@gmail.com>
2026-04-11 20:17:13 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 https://t.me/proxy?server=217.144.189.88&port=443&secret=ee241a8226e64d7764e69649f6760927c37777772e676f6f676c652e636f6d
 https://t.me/proxy?server=rkn.tg&port=1337&secret=dda2d3fa639a6ddcf8d6d1c5913d001531
+https://t.me/proxy?server=apis.eventcalendar.vip&port=443&secret=ee42307b510f9ba46450cc1bda968fcf3d63646e2e6a7364656c6976722e6e6574
