6940e54856e2a9dad1f2963ff6b4857e600e33e5
fengitfl <enikonov229@gmail.com>
2026-04-11 20:12:06 +0300

    Update __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+https://t.me/proxy?server=217.144.189.88&port=443&secret=ee241a8226e64d7764e69649f6760927c37777772e676f6f676c652e636f6d
+https://t.me/proxy?server=rkn.tg&port=1337&secret=dda2d3fa639a6ddcf8d6d1c5913d001531
